aa67a338952cfbcbd65aa2920497924e6e09d9c9
wangzhenhui1992 <605951224@qq.com>
2018-12-17 14:46:09 +0900

    添加meta信息做seo优化

--- src/app/page/articlepage/articlepage.component.ts
+++ src/app/page/articlepage/articlepage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PageComponent } from '../page.component';
-import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
+import { SafeHtml, DomSanitizer, Title, Meta } from '@angular/platform-browser';
 import { ArticleService } from '../../service/article.service';
 import PagePath from '../../util/pagepath';
 import { Article } from '../../entity/article';
@@ -19,7 +19,7 @@ export class ArticlepageComponent extends PageComponent implements OnInit {
   safeBody: SafeHtml;
 
   constructor(private router: Router, private activedRouter: ActivatedRoute, private articleService: ArticleService,
-    private markdownService: MarkdownService) {
+    private markdownService: MarkdownService, private title: Title, private meta: Meta) {
     super();
   }
 
@@ -39,7 +39,10 @@ export class ArticlepageComponent extends PageComponent implements OnInit {
         this.router.navigate([PagePath.ERROR_PAGE]);
         return;
       }
+      this.title.setTitle(this.article.title);
       this.safeBody = this.markdownService.render(this.article.content);
+      this.meta.getTag('name="description"').setAttribute("content",this.article.summary);
+      this.meta.addTag({ name: 'keywords', content: this.article.labels.join(",")+","+this.article.category});
       // this.safeBody = this.domSanitizer.bypassSecurityTrustHtml(markdown.toHTML(this.article.content));
 
     });
